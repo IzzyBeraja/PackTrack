bbfa58c06eee052ea75b0ae4628bdf287db89402
IzzyBeraja <izzyberaja+github@gmail.com>
2020-12-02 14:47:26 -0500

    Changing to TS and added a base layout 📊

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav className={styles.nav}></nav>
+      <div className={styles.sidebar}></div>
+      <main className={styles.main}></main>
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
