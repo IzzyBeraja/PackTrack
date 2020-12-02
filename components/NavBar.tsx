ed74e3f19c96004d1c7c76227818bb735973a813
IzzyBeraja <izzyberaja+github@gmail.com>
2020-12-02 17:07:19 -0500

    Created a Navbar 🧭

---
+++ components/NavBar.tsx
@@ -0,0 +1,17 @@
+import styles from "./NavBar.module.css";
+
+type Props = {
+  className?: React.ReactNode;
+};
+
+const NavBar = ({ className }: Props) => {
+  return (
+    <nav className={`${className ?? ""} navbar ${styles.container}`}>
+      <a className={`navbar-brand ${styles.brand}`} href="#">
+        PackTrack
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
